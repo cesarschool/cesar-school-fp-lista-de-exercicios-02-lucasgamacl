a2a7bc51527d3cf60754a2a923299d7e75a0d5e6
lucasgamacl <38013162+lucasgamacl@users.noreply.github.com>
2018-05-12 11:47:17 -0300

    Update questao_4.py

--- questoes/questao_4.py
+++ questoes/questao_4.py
@@ -14,7 +14,19 @@
 # substituindo apenas o comando print(questão...) existente.
 ##
 def main():
-    print("questao 4")
+    house = int(input('Qual o preço da casa?: '))
+    pay = int(input('Qual o seu salário?: '))
+    months = int(input('Em quantos meses a divida será paga?: '))
+    x = (house/months)
+    y = ((pay/100)*30)
+    if (x>=y):
+        print('Você não pode pagar a casa!')
+        print('As parcelas custam: ', x)
+        print('30% do seu salário é: ', y)
+    else:
+        print('Você pode comprar a casa!')
+        print('As parcelas custam: ', x)
+        print('30% do seu salário é: ', y)
 
 
     
